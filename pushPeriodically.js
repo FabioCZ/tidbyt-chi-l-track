fd6a68d172ad93d106fac0cbc655cea85b877085
Fabio Gottlicher <g.fabio.cz@gmail.com>
2023-10-04 20:38:03 -0500

    initial

---
+++ pushPeriodically.js
@@ -0,0 +1,15 @@
+const { exec } = require("child_process");
+
+const token = ""
+const deviceId = ""
+
+function pushPixlet() {
+	exec("pixlet render chi_l_track.star", (err, out, stderr) => {
+		exec("pixlet push --api-token " + token + " --installation-id chi_l_track " + deviceId + " chi_l_track.webp", (err1, out1, stderr1) => {
+
+		})
+	})
+}
+
+pushPixlet()
+setInterval(pushPixlet, 1000 * 30)
